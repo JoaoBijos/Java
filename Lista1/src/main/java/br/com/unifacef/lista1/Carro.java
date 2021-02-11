500ba70ae5fe369b4fc4204cac1199bd4308fc95
JoaoBijos <joaopedrobijos96@gmail.com>
2021-02-10 22:19:16 -0300

    Primeira aula

---
+++ Lista1/src/main/java/br/com/unifacef/lista1/Carro.java
@@ -0,0 +1,13 @@
+package br.com.unifacef.lista1;
+
+public class Carro {
+    
+    // declaração das variáveis 
+    // Java possui tipagem estática, é forte
+    String marca; // String é uma classe, letra maiuscula 
+    String modelo; // String é uma classe, letra maiuscula 
+    int ano; // int é um tipo primitivo, letra minuscula 
+    boolean motor; // boolean é um tipo primitivo, letra minuscula 
+    float velAtual; // float é um tipo primitivo, letra minuscula  
+    
+}
